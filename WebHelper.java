5717fcfb37b4bc99a596d79af96bffccf54c1133
Benjamin Meshanko <70403274+Bmeshanko@users.noreply.github.com>
2021-12-16 22:44:38 -0500

    Create WebHelper.java

---
+++ WebHelper.java
@@ -0,0 +1,14 @@
+// Class to Simply PrintWriter calls.
+public class WebHelper {
+	public String init = "<!DOCTYPE html>\n<html>\n<head>\n"
+			+ "<link rel=\"stylesheet\" href=\"style.css\">\n</head>\n<body>";
+	public String close = "</body>\n</html>";
+	
+	public String elo = "These ratings are calculated using the Elo rating system. "
+			+ "Elo is commonly used in chess<br> where players can either win, lose or draw. "
+			+ "However, sports can have blowout wins or<br> close losses. These ratings account" + 
+			" for that by calculating an expected score, and taking<br> the difference from that as a variable.";
+	public WebHelper() {
+		
+	}
+}
